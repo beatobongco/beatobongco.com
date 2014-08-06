25322e9574c68f95a61a9534e7ee4213aa91261a
Beato Bongco <beatobongco@gmail.com>
2014-08-07 03:53:21 +0800

    root path shows the titles of all articles you made and links it to the correct route

--- pyrachnid/views.py
+++ pyrachnid/views.py
@@ -3,9 +3,11 @@ from app import app
 
 import os
 
-# Functions
+#Variables
+static_directory = os.path.dirname(os.path.abspath(__file__)) + "/static/"
+pages_path = "pages/"
+pages_directory = static_directory + pages_path
 
-#Dict for replacement
 replacement = [ 
   ['[b]' , '<b>'],
   ['[/b]' , '</b>'],
@@ -25,6 +27,9 @@ replacement = [
   ['[/' , '</'],
 ]
 
+# Functions
+
+#Dict for replacement
 def replace_all(text, dic):
   for i, j in replacement:
     text = text.replace(i, j)
@@ -33,19 +38,36 @@ def replace_all(text, dic):
 @app.route("/")
 
 def index():
-  return "Hello World!!"
+  # get all titles of blog entries
+  all_articles = []
+
+  for file in os.listdir(pages_directory):
+    if file.endswith(".txt"):
+      try:
+        with open(pages_directory+file, 'r') as myarticle:
+          app.logger.debug("!")
+          all_articles.append([file.replace('.txt', ''), myarticle.readline()])
+      except IOError:
+        pass
+
+  app.logger.debug(all_articles)
+  temp_return = ""
+
+  for file, article in all_articles:
+    temp_return = temp_return + "<br>" + "<a href='" + pages_path + file + "'>" + article + "</a>"
+
+  return render_template('pages.html', title="Your Site's Name", content=temp_return)
 
 @app.route("/pages/<article>")
 
 def show_page(article):
   # parsing code goes here
-  full_directory = os.path.dirname(os.path.abspath(__file__))
   title = ""
   content = ""
 
   article_url = url_for('static', filename='pages/' + article + '.txt') 
   try:
-    with open(full_directory + article_url, 'r') as myarticle:
+    with open(pages_directory + article + ".txt", 'r') as myarticle:
       file_content = myarticle.readlines()
   except IOError:
     abort(404)
